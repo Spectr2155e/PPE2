d3f0b357b61e1242d31306db8a4901e2f7ca501e
Spectr2155e <99422543+Spectr2155e@users.noreply.github.com>
2024-10-23 18:21:36 +0200

    Delete keys_orga.sql

--- keys_orga.sql
+++
@@ -1,9 +0,0 @@
-create table keys_orga
-(
-    ID              int auto_increment
-        primary key,
-    KEY_STRING      text null,
-    ORGANISATION_ID int  null
-);
-
-INSERT INTO ppe2.keys_orga (ID, KEY_STRING, ORGANISATION_ID) VALUES (1, 'HUuHXILF1b9UiRHqQzic', 1);
